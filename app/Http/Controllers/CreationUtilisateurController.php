4fbaee53efc1075ea437bf9f0739eb7fb4e8a775
cherifyounouss <chinsanity16@gmail.com>
2020-02-17 22:04:08 +0000

    controller to create user with profile updated

--- app/Http/Controllers/CreationUtilisateurController.php
+++ app/Http/Controllers/CreationUtilisateurController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CreationUtilisateurRequest;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 use App\Utilisateur;
 
 
@@ -23,16 +25,28 @@ class CreationUtilisateurController extends Controller
         $date_naissance = $data['date_naissance'];
         $num_tel = $data['numero_telephone'];
         $profil = $data['profil'];
+        $email = $data['email'];
+        $password = Hash::make($data['password']);
 
         $utilisateur = new Utilisateur([
             'prenom' => $prenom,
             'nom' => $nom,
             'date_naissance' => $date_naissance,
             'numero_telephone' => $num_tel,
-            'profil' => $profil
+            'profil' => $profil,
         ]);
 
         $utilisateur->save();
+
+        $user = new User([
+            'id' => $utilisateur->id,
+            'email' => $email,
+            'password' => $password
+        ]);
+        $user->save();
         echo "Userrr created successfully";
     }
+
+
+
 }
