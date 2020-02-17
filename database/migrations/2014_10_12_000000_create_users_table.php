8cfbbb29d0437d3de5d9cff5b45ee30ec8349cd5
cherifyounouss <chinsanity16@gmail.com>
2020-02-17 22:05:23 +0000

    account migration updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::enableForeignKeyConstraints();
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+            $table->unsignedBigInteger('id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('id')->references('id')->on('utilisateurs');
         });
     }
 
