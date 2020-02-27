d873ace12b72053dd75494811607337f3257000d
cherifyounouss <chinsanity16@gmail.com>
2020-02-27 10:34:48 +0000

    controller etudiant

---
+++ app/Http/Controllers/EtudiantController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Etudiant;
+use Illuminate\Http\Request;
+use App\Imports\EtudiantsImport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class EtudiantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('regisseur.upload_liste_etudiants');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Etudiant  $etudiant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Etudiant  $etudiant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Etudiant  $etudiant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Etudiant  $etudiant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Etudiant $etudiant)
+    {
+        //
+    }
+
+        /**
+     * Import function
+     */
+    public function import(Request $request)
+    {
+        if ($request->file('imported_file')) {
+            Excel::import(new EtudiantsImport(), request()->file('imported_file'));
+            return back();
+        }
+    }
+}
