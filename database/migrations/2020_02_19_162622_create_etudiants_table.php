9df0f223e609d1216a77808183bdb247d66c93c8
cherifyounouss <chinsanity16@gmail.com>
2020-02-27 10:35:09 +0000

    migration create etudiant

---
+++ database/migrations/2020_02_19_162622_create_etudiants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('prenom');
+            $table->string('nom');
+            $table->unsignedBigInteger('cni');
+            $table->string('ine');
+            $table->string('niveau');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+}
