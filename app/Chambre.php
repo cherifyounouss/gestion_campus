91f0bac0b9c2abb5234320b7870e38a867617485
cherifyounouss <chinsanity16@gmail.com>
2020-03-21 01:14:25 +0000

    updated fillable field

--- app/Chambre.php
+++ app/Chambre.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chambre extends Model
 {
-    //
+    protected $fillable = [
+        'numero', 'disponible', 'contrainteSexe', 'contrainteNiveauEtude', 'idEtage',
+    ];
+    public $timestamps = false;
 }
